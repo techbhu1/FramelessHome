bc579aedf4fab4c22348851f4176cfcecf831f62
techbhu1 <techbhu1@gmail.com>
2024-03-04 09:03:16 +0530

    aos help v1
    v1

---
+++ whxdata/idx.new.js
@@ -0,0 +1,4 @@
+(function() {
+var index =  {"type":"index","chunkinfos":[{"type":"chunkinfo","first":"{index}flow{default para font} command","last":"VLANs","num":"42","node":"idata1"}]};
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), index, { sync:true });
+})();
